a389869cff201da8fd02ce4e8fd025c3d4a7534d
Arvinder Singh <sardarchitect@gmail.com>
2023-07-22 20:15:37 -0400

    Updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,23 @@
-export default function Footer () {
-    return(
-        <footer className='flex justify-center'>
-          <p className='mt-5'>Arvinder Singh - 2023</p>
-        </footer>
-    )
+import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className='mt-5 flex justify-center gap-6'>
+      <Link href="mailto:singh@arvinder.me" className="flex gap-3 align-middle align">
+        <FontAwesomeIcon icon={faEnvelope} color="white" />
+        <p>Email</p>
+      </Link>
+      <Link href="https://github.com/sardarchitect" className="flex gap-3 align-middle align">
+        <FontAwesomeIcon icon={faGithub} color="white" />
+        <p>Github</p>
+      </Link>
+      <Link href="https://www.linkedin.com/in/sardarchitect/" className="flex gap-3 align-middle align">
+        <FontAwesomeIcon icon={faLinkedin} color="white" />
+        <p>LinkedIn</p>
+      </Link>
+    </footer>
+  )
 }
